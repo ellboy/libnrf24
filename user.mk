76460b743c0e154a9bf7b50a013b8bc223031e7d
ellboy <reg@online.de>
2017-08-28 22:03:02 +0200

    nRF24L01 library by Rocco Marco Guglielmi http://www.playembedded.org/blog/en/2015/07/16/a-radio-frequency-transceiver-library-nrf24l01-and-chibiosrt/

---
+++ user.mk
@@ -0,0 +1,7 @@
+USERLIB = ./userlib
+# List of all the Userlib files
+USERSRC =  $(USERLIB)/src/rf.c 
+          
+# Required include directories
+USERINC =  $(USERLIB) \
+           $(USERLIB)/include 
